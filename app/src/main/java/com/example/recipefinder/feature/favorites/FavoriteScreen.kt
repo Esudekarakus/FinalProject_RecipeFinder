14bbafe79939a53eb13a588e8dcc93f85ac7b9ee
Esudekarakus <145253733+Esudekarakus@users.noreply.github.com>
2024-06-12 21:06:36 +0300

    Favoriler ekranı son tasarım

--- app/src/main/java/com/example/recipefinder/feature/favorites/FavoriteScreen.kt
+++ app/src/main/java/com/example/recipefinder/feature/favorites/FavoriteScreen.kt
@@ -5,16 +5,10 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
@@ -22,6 +16,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -43,6 +39,7 @@ fun FavoriteScreen(
         topBar = {
             TopAppBar(
                 title = { Text("Favorite Recipes") },
+                colors = TopAppBarDefaults.topAppBarColors(Color(0xFFFF7F50)),
                 navigationIcon = {
                     IconButton(onClick = {
                         navController.popBackStack()
@@ -84,10 +81,13 @@ fun RecipeItem(recipe: RecipeModel, onRecipeClick: (Int) -> Unit) {
             .fillMaxWidth()
             .padding(8.dp)
             .clickable { onRecipeClick(recipe.id) },
-
-        ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically
+        shape = RoundedCornerShape(8.dp),
+        elevation = CardDefaults.elevatedCardElevation(8.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White)
+    ) {
+        Column(
+            modifier = Modifier.padding(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             val imageSize = with(LocalDensity.current) { 120.dp.toPx().toInt() }
             Image(
@@ -96,16 +96,15 @@ fun RecipeItem(recipe: RecipeModel, onRecipeClick: (Int) -> Unit) {
                 contentScale = ContentScale.Crop,
                 modifier = Modifier.size(imageSize.dp)
             )
-            Column(
-                modifier = Modifier
-                    .padding(horizontal = 16.dp, vertical = 8.dp)
-            ) {
-                Text(
-                    text = recipe.title,
-                    fontSize = 18.sp,
-                    modifier = Modifier.weight(1f)
-                )
-            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = recipe.title,
+                fontSize = 18.sp,
+                textAlign = TextAlign.Center,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier.fillMaxWidth()
+            )
         }
     }
 }
